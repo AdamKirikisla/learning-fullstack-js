45d99228d5d3622fb3488fb5265f4d8f79a8f5de
AdamKirikisla <280528387+AdamKirikisla@users.noreply.github.com>
2026-09-05 15:10:54 -0400

    add Footer.jsx

---
+++ learning-fullstack-js/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer(){
+    return(
+        <footer>
+            <div className="footer"> 
+            <p>Finish the build before you move on.</p>
+            <p>learning-fullstack-js</p>
+
+            </div>
+            
+        </footer>
+    )
+}
