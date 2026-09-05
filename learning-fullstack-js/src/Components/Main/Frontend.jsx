2e5572cd7c23423f38899be9e806bf2819b19ddc
AdamKirikisla <280528387+AdamKirikisla@users.noreply.github.com>
2026-09-05 14:57:32 -0400

    add Frontend phase

---
+++ learning-fullstack-js/src/Components/Main/Frontend.jsx
@@ -0,0 +1,15 @@
+export default function Frontend(){
+    return(
+                <section className="phase">
+                  <div className="phase-desc">
+                    <p>PHASE 03</p>
+                    <h2>Frontend</h2>
+                    <p>---</p>
+                  </div>
+                  <div className="phase-grid">
+                    
+                    
+                  </div>
+                </section>
+            )
+}
--- learning-fullstack-js/src/Components/Main/Main.jsx
+++ learning-fullstack-js/src/Components/Main/Main.jsx
@@ -1,11 +1,13 @@
 import Foundations from "./Foundations"
 import Backend from "./Backend"
+import Frontend from "./Frontend"
 
 export default function Main(){
     return(
         <section>
         <Foundations />
         <Backend />
+        <Frontend />
         </section>
 
     )
